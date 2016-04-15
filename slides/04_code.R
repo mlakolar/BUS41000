d2987b422427a98cb1d3dfbf13bc6591a67f2919
Mladen Kolar <mkolar@gmail.com>
2016-04-15 13:56:00 -0500

    week 04 code

--- slides/04_code.R
+++ slides/04_code.R
@@ -16,7 +16,7 @@ source('https://raw.githubusercontent.com/mlakolar/BUS41000/master/BUS41000.pack
 setwd("/home/mkolar/Downloads/")
 
 ############################### 
-# 
+# Fraud data
 ###############################
 
 
@@ -32,6 +32,13 @@ header = scan("fraud.csv", nlines = 1, what = character(), sep=",")
 header2 = scan("fraud.csv", skip = 1, nlines = 1, what = character(), sep=",")
 
 fraud_df = read.csv("fraud.csv", skip = 2, header = FALSE, row.names = 1)
-names(fraud_df) = paste0(header[-1], "/", header2[-1])
+names(fraud_df) = c("Credit History", "Guarantor CoApplicant","Accommodation","Fraud")
 
 head(fraud_df)
+
+table(fraud_df$`Fraud`, dnn = names(fraud_df)[4], deparse.level = 1)
+table(fraud_df$`Fraud`, fraud_df$`Credit History`, dnn = names(fraud_df)[c(1,4)], deparse.level = 1)
+table(fraud_df$`Fraud`, fraud_df$`Guarantor CoApplicant`, dnn = names(fraud_df)[c(2,4)], deparse.level = 1)
+table(fraud_df$`Fraud`, fraud_df$`Accommodation`, dnn = names(fraud_df)[c(3,4)], deparse.level = 1)
+
+
